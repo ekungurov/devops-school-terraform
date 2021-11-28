cf93298ce31ab6a555b33da1f347856d9cd68ac6
Yevgeny Kungurov <ekungurov83@gmail.com>
2021-11-28 16:28:16 +0400

    Add ec2-instance directory

---
+++ ec2-instance/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-030e490c34394591b"
+  instance_type   = "t2.micro"
+  security_groups = ["default", "epam-by-ru"]
+
+  tags = {
+    Name  = "YK-ExampleInstance"
+    owner = "yevgeny_kungurov@epam.com"
+  }
+}
